a9a47ce9e10fd1c01cd1731261956828112db870
Angus McLean <angus.mclean@mail.mcgill.ca>
2016-07-05 14:12:08 -0400

    sync changes

--- js/cue/index.js
+++ js/cue/index.js
@@ -8,6 +8,9 @@
 		setTimeout(chat_module.updateOrStart.bind(chat_module), 500);
 
 		setInterval(chat_module.updateActiveEmployees.bind(chat_module), 10000);
+		setInterval(function () {
+			if(chat_module.active.chat.RTCSession.role === 'HOST') chat_module.active.chat.resyncSessionRecord();
+		}, 5000);
 
 	};
 })();
